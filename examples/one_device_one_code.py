d1187e5c23665bef3be228d9f59c1161f518cb44
sun589 <goodluck1787@outlook.com>
2025-03-09 17:19:10 +0800

    add example.

---
+++ examples/one_device_one_code.py
@@ -0,0 +1,31 @@
+# 这里拿base64做演示,实际场景中请更换加密算法,这里仅简单演示
+import AfdianBot
+import base64
+
+
+def calc_code(code):
+    return base64.b64encode(code.encode()).decode()
+
+
+bot = AfdianBot.Bot(account="xxx", password="xxxx")  # 实例化一个bot
+
+
+@bot.register("/calc (\S+)")
+def test1(msg: AfdianBot.types.TextMsg, code):
+    # 使用api.query_sponsor检查发送者是否到指定金额档位
+    # 如果你想判断发送者是否赞助过某一个方案,可以在plans中检查
+    """
+    for i in AfdianBot.api.query_sponsor(msg.sender_id)['plans']:
+        if i['plan_id'] == "xxx":
+            bot.send_msg(f"你的激活码是:{calc_code(code)}",msg.sender_id)
+            break
+    else:
+        bot.send_msg("你没有赞助xxx方案!",msg.sender_id)
+    """
+    if AfdianBot.api.query_sponsor(msg.sender_id)['all_sum_amount'] >= 20:
+        bot.send_msg(f"你的激活码是:{calc_code(code)}", msg.sender_id)
+    else:
+        bot.send_msg("你没有赞助够20元!", msg.sender_id)
+
+
+bot.run()
